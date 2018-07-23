6fbb194b2e74127e8997e5adc476a5552059020b
Michael Blackley <michael.blackley@rea-group.com>
2018-07-24 08:56:44 +1000

    add redux for state

---
+++ src/state/reducer.js
@@ -0,0 +1,12 @@
+import initialState from './initialState';
+
+const reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    default:
+      return state;
+  }
+
+};
+
+export default reducer;
---
+++ src/store/createStore.js
@@ -0,0 +1,20 @@
+const createStore = (reducer, initialState) => {
+  const store = {};
+  store.state = initialState;
+  store.listeners = [];
+
+  store.getState = () => store.state;
+
+  store.subscribe = listener => {
+    store.listeners.push(listener);
+  };
+
+  store.dispatch = action => {
+    store.state = reducer(store.state, action);
+    store.listeners.forEach(listener => listener());
+  };
+
+  return store;
+};
+
+export default createStore;
