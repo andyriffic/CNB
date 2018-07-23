86272deb94b73e057e7cc3a113870ac9b513dddb
Andrew Bell <andrew.bell@rea-group.com>
2018-07-24 09:33:27 +1000

    Add psudeo-code for game structure in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('Hello cowboy');
+
+/*
+  CONNECT (name)
+    -> check for slot => [Full error]
+      -> Allocate => [Player Connected]
+
+    <- [Ready for moves]
+
+  MAKE MOVE
+    -> validate => [Error]
+      -> store
+    <- Player Selected
+
+  READY TO PLAY
+    <- Result
+*/
