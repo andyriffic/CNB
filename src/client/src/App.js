cba20e86fa863f40e8a06a43c49775ecc587ce86
Michael Blackley <michael.blackley@rea-group.com>
2018-11-13 11:57:26 +1100

    socket connection and state

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,11 +1,28 @@
 /* @flow */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Router } from "@reach/router"
+import socketIOClient from "socket.io-client";
 
 import { PlayerSelectionScreen, SpectatorScreen } from './screens'
 import GlobalStyle from './GlobalStyle';
 
+//todo querystring param
+const debug = true;
+
 const App = () => {
+  const [gameState, setGameState] = useState({});
+  const [connectionDetails, setConnectionDetails] = useState({});
+
+  useEffect(()=> {
+    const socket = socketIOClient();
+    socket.on("CONNECTION_ESTABLISHED", data => setConnectionDetails(data ));
+    socket.on("GAME_STATUS", data => setGameState(data));
+    console.log('connected to socket', socket);
+
+    return () => console.log('unmounting....');
+
+  }, []);
+
   return (
     <React.Fragment>
       <GlobalStyle />
@@ -15,6 +32,19 @@ const App = () => {
         <PlayerSelectionScreen path="xian" playerKey={ 'XIAN' } />
         <PlayerSelectionScreen path="melb" playerKey={ 'MELB' } />
       </Router>
+      { debug && (
+          <React.Fragment>
+            <h2>Client connection</h2>
+            <pre>
+              { JSON.stringify(connectionDetails) }
+            </pre>
+            <h2>Game state</h2>
+            <pre>
+              { JSON.stringify(gameState) }
+            </pre>
+          </React.Fragment>
+        )
+      }
     </React.Fragment>
   );
 }
