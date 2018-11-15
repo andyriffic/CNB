fe60427d2aba0d3b5e1d66c6c2b66708bdc062b3
Michael Blackley <michael.blackley@rea-group.com>
2018-11-16 09:28:38 +1100

    delay results on the player screen

--- src/client/src/screens/player-selection-screen/View.js
+++ src/client/src/screens/player-selection-screen/View.js
@@ -7,9 +7,7 @@ import usePlayerState from './hooks/usePlayerState';
 import DebugOutput from '../../DebugOutput';
 import SelectMove from './components/select-move';
 import SelectedMove from './components/selected-move';
-import Winner from './components/outcome-winner';
-import Loser from './components/outcome-loser';
-import Draw from './components/outcome-draw';
+import GameResult from './components/game-result';
 import ServerMessagesContext from '../../contexts/ServerMessagesContext';
 import GameStateContext from '../../contexts/GameStateContext';
 import Switch from '../../components/switch';
@@ -23,10 +21,7 @@ type Props = {
 };
 
 const hasGameResult = (gameState) => !!(gameState && gameState.result);
-const gameIsDraw = (gameState) => hasGameResult(gameState) && gameState.result.draw;
 const playerHasMoved = (gameState, playerState) => playerState && playerState.player && playerState.player.moved;
-const playerWins = (gameState, player) => !gameIsDraw(gameState) && hasGameResult(gameState) && gameState.result.winner === player;
-const playerLoses = (gameState, player) => !gameIsDraw(gameState) && hasGameResult(gameState) && gameState.result.winner !== player;
 
 const View = ( { playerKey }: Props ) => {
   const playerState = usePlayerState(playerKey);
@@ -41,12 +36,6 @@ const View = ( { playerKey }: Props ) => {
 
   if (!playerState) return null;
 
-  // console.log('hasGameResult', hasGameResult(gameState));
-  // console.log('gameIsDraw', gameIsDraw(gameState));
-  // console.log('playerHasMoved', playerHasMoved(gameState, playerState));
-  // console.log('playerWins', playerWins(gameState, playerState.slot));
-  // console.log('playerLoses', playerLoses(gameState, playerState.slot));
-
   return (
     <Page>
       <PageHeader>
@@ -59,15 +48,10 @@ const View = ( { playerKey }: Props ) => {
             showIf={ !hasGameResult(gameState) && playerHasMoved(gameState, playerState) }
             title="You chose 你选择了"
             selectedMove={ playerState.player.move }/>
-          <Draw
-            showIf={ gameIsDraw(gameState) }
-            selectedMove={ playerState.player.move }/>
-          <Loser
-            showIf={ playerLoses(gameState, playerState.slot) }
-            selectedMove={ playerState.player.move }/>
-          <Winner
-            showIf={ playerWins(gameState, playerState.slot) }
-            selectedMove={ playerState.player.move }/>
+          <GameResult
+            showIf={ hasGameResult(gameState) }
+            gameState={ gameState }
+            playerState={ playerState } />
         </Switch>
       </PageBody>
       <DebugOutput data={ playerState } />
---
+++ src/client/src/screens/player-selection-screen/components/game-result/View.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+import Switch from '../../../../components/switch';
+import Winner from '../outcome-winner';
+import Loser from '../outcome-loser';
+import Draw from '../outcome-draw';
+import WaitingIcon from '../../../../components/icons/loading';
+
+const gameIsDraw = (gameState) => gameState.result.draw;
+const playerWins = (gameState, player) => !gameIsDraw(gameState) && gameState.result.winner === player;
+const playerLoses = (gameState, player) => !gameIsDraw(gameState) && gameState.result.winner !== player;
+
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const FadeInContainer = styled.div`
+  opacity:0;
+  animation: ${fadeIn} 2s 1 forwards;
+`
+
+const View = ({gameState, playerState}) => {
+    const [isWaiting, setIsWaiting] = useState(true);
+
+    useEffect(() => {
+      setTimeout(() => {
+        setIsWaiting(false);
+      }, 7000)
+    }, []);
+
+    return (
+        <React.Fragment>
+          {
+            isWaiting ?
+            (
+              <React.Fragment>
+                <WaitingIcon animationDelay={0} />
+                <h3>Running game 跑步比賽</h3>
+              </React.Fragment>
+            ) : (
+              <FadeInContainer>
+                <Switch>
+                  <Draw
+                    showIf={ gameIsDraw(gameState) }
+                    selectedMove={ playerState.player.move }/>
+                  <Loser
+                    showIf={ playerLoses(gameState, playerState.slot) }
+                    selectedMove={ playerState.player.move }/>
+                  <Winner
+                    showIf={ playerWins(gameState, playerState.slot) }
+                    selectedMove={ playerState.player.move }/>
+                </Switch>
+              </FadeInContainer>
+            )
+          }
+        </React.Fragment>
+    )
+}
+
+export default View;
