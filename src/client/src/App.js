95dfdc4f824ed884df3355167fa983f1637e6829
Michael Blackley <michael.blackley@rea-group.com>
2018-11-16 11:15:09 +1100

    environment banner

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,6 +1,7 @@
 /* @flow */
 // flow:disable no typedefs for useState, useEffect yet
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import { Router } from "@reach/router"
 
 import SocketsConnection from './sockets/SocketsConnection';
@@ -10,6 +11,26 @@ import DebugOutput from './DebugOutput';
 import GlobalStyle from './GlobalStyle';
 import ScoreboardApi from './scoreboard/ScoreboardApi';
 
+import { IS_PRODUCTION } from './environment';
+
+const FullScreen = styled.div`
+  height: 100vh;
+  width: 100vw;
+`;
+
+const EnvironmentBanner = styled.div`
+  position: fixed;
+  bottom: 0;
+  height: 2%;
+  background-color: ${props => props.isProduction ? 'green': 'red' };
+  color: white;
+  font-size: .5rem;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
 const App = () => {
   const connectionDetails = useContext(ConnectionDetailsContext);
 
@@ -17,13 +38,20 @@ const App = () => {
     <ScoreboardApi>
       <SocketsConnection>
         <GlobalStyle />
-        <Router>
-          <SpectatorScreen path="/" />
-          <PlayerSelectionScreen path="xian" playerKey={ 'XIAN' } />
-          <PlayerSelectionScreen path="melb" playerKey={ 'MELB' } />
-          <ResetGameScreen path="reset" />
-        </Router>
-        <DebugOutput data={connectionDetails} />
+        <FullScreen>
+          <Router>
+            <SpectatorScreen path="/" />
+            <PlayerSelectionScreen path="xian" playerKey={ 'XIAN' } />
+            <PlayerSelectionScreen path="melb" playerKey={ 'MELB' } />
+            <ResetGameScreen path="reset" />
+          </Router>
+          <DebugOutput data={connectionDetails} />
+          <EnvironmentBanner isProduction={IS_PRODUCTION}>
+            {
+              IS_PRODUCTION ? 'Production' : 'Test'
+            }
+          </EnvironmentBanner>
+        </FullScreen>
       </SocketsConnection>
     </ScoreboardApi>
   );
