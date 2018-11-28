b2cf8c4b0f7825fb79e79e73639210d779d79b17
FooDog <andyriffic@gmail.com>
2018-11-28 21:43:45 +1100

    Start playing sound if music turned back on

--- src/client/src/sounds/soundService.js
+++ src/client/src/sounds/soundService.js
@@ -20,43 +20,62 @@ export const SOUND_KEYS = {
 export class SoundService {
 
   _sounds = {};
+  _resumableSoundKeys = []; //Sounds that can be played when music is toggled back on
   _musicEnabled: false;
 
   constructor(musicEnabled) {
     this._musicEnabled = musicEnabled;
 
     //Pre-load sounds
-    this._sounds[SOUND_KEYS.WAITING_MUSIC] = new Howl({
-      src: [waitingSound],
-      loop: true,
-      volume: 0.6,
-    });
+    this._sounds[SOUND_KEYS.WAITING_MUSIC] = {
+      resumeable: true,
+      sound: new Howl({
+        src: [waitingSound],
+        loop: true,
+        volume: 0.6,
+      }),
+    };
   }
 
   setMusicEnabled(enabled) {
     this._musicEnabled = enabled;
 
+
+    if (enabled) {
+      this._resumableSoundKeys.forEach((soundKey) => {
+        this._sounds[soundKey].sound.play();
+      });
+    }
+
+
     if (!enabled) {
       Object.keys(this._sounds).forEach((soundKey) => {
-        this._sounds[soundKey].stop();
-      })
+        this._sounds[soundKey].sound.stop();
+      });
     }
   }
 
   play(soundKey) {
+
+    //Place sound in resumable sounds in case music gets turned on
+    if (this._sounds[soundKey].resumeable && !this._resumableSoundKeys.includes(soundKey)) {
+      this._resumableSoundKeys = [...this._resumableSoundKeys, soundKey];
+    }
+
     if (!this._musicEnabled) {
       return;
     }
 
-    if (this._sounds[soundKey].playing()) {
+    if (this._sounds[soundKey].sound.playing()) {
       return;
     }
 
-    this._sounds[soundKey].play();
+    this._sounds[soundKey].sound.play();
   }
 
   stop(soundKey) {
-    this._sounds[soundKey].stop();
+    this._resumableSoundKeys = this._resumableSoundKeys.filter((key) => key != soundKey);
+    this._sounds[soundKey].sound.stop();
   }
 
 }
