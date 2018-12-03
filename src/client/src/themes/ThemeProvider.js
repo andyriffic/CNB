df4142ccbc91d3052af0383c63d64eb4ef737cd5
Andrew Bell <andrew.bell@rea-group.com>
2018-12-04 10:54:46 +1100

    Automatically set xmas theme if in December

--- src/client/src/themes/ThemeProvider.js
+++ src/client/src/themes/ThemeProvider.js
@@ -4,9 +4,20 @@ import CowboyNinjaBearTheme from './cowboy-ninja-bear';
 import XmasTheme from './xmas';
 import { isFeatureEnabled } from '../featureToggle';
 
+const DECEMBER_MONTH = 11; //javascript months are 0 based :)
+
+const isDecemberAndBeforeChristmasDay = () => {
+  const today = new Date();
+
+  const month = today.getMonth();
+  const day = today.getDate();
+
+  return month === DECEMBER_MONTH && day <= 25;
+}
+
 const View = ({children}) => {
 
-  const theme = isFeatureEnabled('xmas') ? XmasTheme : CowboyNinjaBearTheme;
+  const theme = isDecemberAndBeforeChristmasDay() ? XmasTheme : CowboyNinjaBearTheme;
 
   return (
     <GameThemeContext.Provider value={theme}>
