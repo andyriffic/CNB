f80f8d2c7e03b4fcc969acd607278a738f41816f
FooDog <andyriffic@gmail.com>
2018-12-25 09:12:58 +1100

    Pulse symbols on spectator waiting screen

--- src/client/src/screens/spectator-screen/components/intro-banner/View.js
+++ src/client/src/screens/spectator-screen/components/intro-banner/View.js
@@ -18,6 +18,20 @@ const extremeFadeAndScale = keyframes`
     }
 `;
 
+const pulse = keyframes`
+  from {
+    transform: scale3d(1, 1, 1);
+  }
+
+  50% {
+    transform: scale3d(1.2, 1.2, 1.2);
+  }
+
+  to {
+    transform: scale3d(1, 1, 1);
+  }
+`
+
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
@@ -36,14 +50,14 @@ const PlayerSymbol = styled.div`
   padding: 0 20px;
   opacity: 0;
   animation: ${extremeFadeAndScale} 1s linear ${props => props.animationDelay}ms
-    1 ${props => props.animationDirection};
-  animation-fill-mode: forwards;
+    1 forwards;  
 `;
 
 const Symbol = styled.div`
   margin: 0 auto;
   width: 130px;
   height: 130px;
+  animation: ${pulse} 2s linear ${props => props.pulseDelay}s infinite forwards;
 `;
 
 const View = () => {
@@ -54,9 +68,9 @@ const View = () => {
       {Object.keys(theme.characters.characterMapping).map((key, index) => {
         const Component = theme.characters.characterMapping[key];
         return (
-          <PlayerSymbol key={key} animationDelay={index * 500}>
+          <PlayerSymbol key={key} animationDelay={index * 500} pulseDelay={(index * 1) + 3}>
             <SymbolTitle>{theme.characters.nameMapping[key]}</SymbolTitle>
-            <Symbol>
+            <Symbol pulseDelay={(index * 1) + 8}>
               <Component />
             </Symbol>
           </PlayerSymbol>
