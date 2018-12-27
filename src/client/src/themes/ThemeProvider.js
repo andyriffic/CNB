14a6c54bde49350db622bda9b013ca92828084e0
Andrew Bell <andrew.bell@rea-group.com>
2018-12-28 08:43:31 +1100

    Set PPP to default theme (with 'normal' feature toggle override)

--- src/client/src/themes/ThemeProvider.js
+++ src/client/src/themes/ThemeProvider.js
@@ -1,32 +1,13 @@
-import React from "react";
-import GameThemeContext from "../contexts/GameThemeContext";
-import CowboyNinjaBearTheme from "./cowboy-ninja-bear";
-import PandaPizzaPirate from "./panda-pizza-pirate";
-import XmasTheme from "./xmas";
-import { isFeatureEnabled } from "../featureToggle";
-
-const DECEMBER_MONTH = 11; //javascript months are 0 based :)
-
-const isDecemberAndBeforeChristmasDay = () => {
-  const today = new Date();
-
-  const month = today.getMonth();
-  const day = today.getDate();
-
-  return month === DECEMBER_MONTH && day <= 25;
-};
+import React from 'react';
+import GameThemeContext from '../contexts/GameThemeContext';
+import CowboyNinjaBearTheme from './cowboy-ninja-bear';
+import PandaPizzaPirate from './panda-pizza-pirate';
+import { isFeatureEnabled } from '../featureToggle';
 
 const View = ({ children }) => {
-  let theme;
-  if (isFeatureEnabled("normal")) {
-    theme = CowboyNinjaBearTheme;
-  } else if (isFeatureEnabled("ppp")) {
-    theme = PandaPizzaPirate;
-  } else {
-    theme = isDecemberAndBeforeChristmasDay()
-      ? XmasTheme
-      : CowboyNinjaBearTheme;
-  }
+  const theme = isFeatureEnabled('normal')
+    ? CowboyNinjaBearTheme
+    : PandaPizzaPirate;
 
   return (
     <GameThemeContext.Provider value={theme}>
