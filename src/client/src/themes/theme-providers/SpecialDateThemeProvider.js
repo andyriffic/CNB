56c45b0d224cd299d8efddf6b8c08743dd3fd271
FooDog <andyriffic@gmail.com>
2019-01-29 19:49:31 +1100

    Extend Australia day theme for an extra week

--- src/client/src/themes/theme-providers/SpecialDateThemeProvider.js
+++ src/client/src/themes/theme-providers/SpecialDateThemeProvider.js
@@ -14,9 +14,15 @@ class Provider implements ThemeProvider {
     const month = this._date.getMonth() + 1; // Add 1 since getMonth returns 0 based index
     const date = this._date.getDate();
 
-    if (month === 1 && date === 29) {
+    if (
+      (month === 1 && date === 29) ||
+      (month === 2 && (date >= 4 && date <= 8))
+    ) {
       return AustraliaTheme;
-    } else if (month === 1 && (date >= 21 && date <= 31)) {
+    } else if (
+      (month === 1 && (date >= 21 && date <= 31)) ||
+      (month === 2 && date === 1)
+    ) {
       return NewYearTheme;
     }
   };
