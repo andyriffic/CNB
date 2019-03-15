2ff9fb91ac7de623ac5ce5cade3381a4ed904708
FooDog <andyriffic@gmail.com>
2019-03-16 07:40:26 +1100

    Add PowerUp appliers test

---
+++ src/client/src/power-ups/appliers.test.js
@@ -0,0 +1,209 @@
+import { applyPowerUpsToPointsAssignment } from './appliers';
+
+describe('PowerUp Appliers', () => {
+  let scores, gameFinishedResult, gameState;
+
+  describe('given player1 is winner', () => {
+    beforeAll(() => {
+      gameFinishedResult = {
+        winnerKey: 'player1',
+        loserKey: 'player2',
+      };
+    });
+
+    describe('when winning with no powerup', () => {
+      beforeAll(() => {
+        gameState = {
+          player1: {
+            powerUp: 'NONE',
+            name: 'P1',
+          },
+          player2: {
+            powerUp: 'NONE',
+            name: 'P2',
+          },
+        };
+      });
+
+      it('then does not change final scores', () => {
+        const currentPointsAssignment = {
+          player1: 0,
+          player2: 0,
+          bonus: 0,
+        };
+        const pointsAssignment = applyPowerUpsToPointsAssignment(
+          currentPointsAssignment,
+          gameFinishedResult,
+          gameState
+        );
+        expect(pointsAssignment).toEqual(currentPointsAssignment);
+      });
+    });
+
+    describe('when winning with DOUBLE POINTS PowerUp', () => {
+      beforeAll(() => {
+        gameState = {
+          player1: {
+            powerUp: 'DOUBLE',
+            name: 'P1',
+          },
+          player2: {
+            powerUp: 'NONE',
+            name: 'P2',
+          },
+        };
+      });
+
+      it('then applies correct PowerUp scores', () => {
+        const currentPointsAssignment = {
+          player1: 1,
+          player2: 0,
+          bonus: 0,
+        };
+        const pointsAssignment = applyPowerUpsToPointsAssignment(
+          currentPointsAssignment,
+          gameFinishedResult,
+          gameState
+        );
+        expect(pointsAssignment).toEqual({
+          player1: 2,
+          player2: 0,
+          bonus: 0,
+        });
+      });
+    });
+
+    describe('when winning with POINTS STEAL PowerUp', () => {
+      beforeAll(() => {
+        gameState = {
+          player1: {
+            powerUp: 'STEAL',
+            name: 'P1',
+          },
+          player2: {
+            powerUp: 'NONE',
+            name: 'P2',
+          },
+        };
+      });
+
+      it('then applies correct PowerUp scores', () => {
+        const currentPointsAssignment = {
+          player1: 1,
+          player2: 0,
+          bonus: 0,
+        };
+
+        const pointsAssignment = applyPowerUpsToPointsAssignment(
+          currentPointsAssignment,
+          gameFinishedResult,
+          gameState
+        );
+        expect(pointsAssignment).toEqual({
+          player1: 2,
+          player2: -1,
+          bonus: 0,
+        });
+      });
+    });
+
+    describe('when winning with POINTS SWAP PowerUp', () => {
+      beforeAll(() => {
+        gameState = {
+          player1: {
+            powerUp: 'SWAP',
+            name: 'P1',
+          },
+          player2: {
+            powerUp: 'NONE',
+            name: 'P2',
+          },
+        };
+      });
+
+      it('then applies correct PowerUp scores when no bonus points', () => {
+        const currentPointsAssignment = {
+          player1: 1,
+          player2: 0,
+          bonus: 0,
+        };
+
+        const scores = {
+          P1: { value: 8 },
+          P2: { value: 10 },
+        };
+
+        const pointsAssignment = applyPowerUpsToPointsAssignment(
+          currentPointsAssignment,
+          gameFinishedResult,
+          gameState,
+          scores
+        );
+        expect(pointsAssignment).toEqual({
+          player1: 2,
+          player2: -2,
+          bonus: 0,
+        });
+      });
+
+      it('then applies correct PowerUp scores when bonus points', () => {
+        const currentPointsAssignment = {
+          player1: 3,
+          player2: 0,
+          bonus: -2,
+        };
+
+        const scores = {
+          P1: { value: 8 },
+          P2: { value: 10 },
+        };
+
+        const pointsAssignment = applyPowerUpsToPointsAssignment(
+          currentPointsAssignment,
+          gameFinishedResult,
+          gameState,
+          scores
+        );
+        expect(pointsAssignment).toEqual({
+          player1: 4,
+          player2: -2,
+          bonus: -2,
+        });
+      });
+    });
+  });
+
+  describe('given result is a draw', () => {
+    beforeAll(() => {
+      gameState = {
+        player1: {
+          powerUp: 'NONE',
+          name: 'P1',
+        },
+        player2: {
+          powerUp: 'NONE',
+          name: 'P2',
+        },
+      };
+
+      gameFinishedResult = {
+        draw: true,
+      };
+    });
+
+    it('then does not alter scores', () => {
+      const currentPointsAssignment = {
+        player1: 0,
+        player2: 0,
+        bonus: 0,
+      };
+
+      const pointsAssignment = applyPowerUpsToPointsAssignment(
+        currentPointsAssignment,
+        gameFinishedResult,
+        scores
+      );
+      expect(pointsAssignment).toEqual(currentPointsAssignment);
+    });
+  });
+});
