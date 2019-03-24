d2be4bd4cac923661d7144e1f94410eaa9844b91
FooDog <andyriffic@gmail.com>
2019-03-24 12:22:21 +1100

    Update text for trophy goal on waiting spectator screen

--- src/client/src/components/trophy-goal/View.js
+++ src/client/src/components/trophy-goal/View.js
@@ -7,14 +7,18 @@ const Container = styled.div`
   margin: 0;
   padding: 0;
   font-size: 1.6rem;
+  display: flex;
+  align-items: center;
 `;
 
 const View = ({ goal }) => {
   return (
     <Container>
+      First to&nbsp;
       <RainbowText>
-        <ReadableNumberFont>{goal}</ReadableNumberFont> 🏆
+        <ReadableNumberFont>{goal}</ReadableNumberFont>
       </RainbowText>
+      &nbsp;gets a 🏆
     </Container>
   );
 };
