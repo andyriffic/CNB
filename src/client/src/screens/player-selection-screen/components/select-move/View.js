c30bc136494a8d2c49717e4cc06031437c824745
FooDog <andyriffic@gmail.com>
2019-04-03 20:53:43 +1100

    Remove feature toggle for swipe Player Selection

--- src/client/src/screens/player-selection-screen/components/select-move/View.js
+++ src/client/src/screens/player-selection-screen/components/select-move/View.js
@@ -3,39 +3,10 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import type { MakeMoveSelection } from '../../types';
-import { media, PageSubTitle } from '../../../styled';
+import { PageSubTitle } from '../../../styled';
 import ItemCardSelection from '../../../../components/item-card-selection';
 import GameThemeContext from '../../../../contexts/GameThemeContext';
-import { FeatureToggle } from '../../../../featureToggle';
 import { PlayerSelectionCard } from './PlayerSelectionCard';
-
-const SelectionList = styled.ul`
-  margin: 0;
-  padding: 0;
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  width: 100%;
-
-  ${media.largeScreen`
-      flex-direction: row;
-      padding: 5px;
-    `}
-`;
-
-const SelectionListItem = styled.li`
-  margin: 0;
-  padding: 0;
-  list-style-type: none;
-  flex-basis: 30%;
-  overflow: hidden;
-  margin-bottom: 10px;
-  flex: 1;
-
-  ${media.largeScreen`      
-      padding: 5px;
-    `}
-`;
 
 const ListContainer = styled.div`
   //width: 100vw;
@@ -56,40 +27,10 @@ const View = ({ onSelection }: MakeMoveSelection) => {
 
   return (
     <React.Fragment>
-      <PageSubTitle>
-        Make your move 做你的動作
-        <FeatureToggle feature="swipe">
-          {toggleEnabled =>
-            !toggleEnabled && (
-              <a href={`${window.location.href}?feature=swipe`}>Swipe</a>
-            )
-          }
-        </FeatureToggle>
-      </PageSubTitle>
-
-      <FeatureToggle feature="swipe">
-        {toggleEnabled =>
-          toggleEnabled ? (
-            <ListContainer>
-              <ItemCardSelection
-                items={items}
-                onItemSelected={selectCharacter}
-              />
-            </ListContainer>
-          ) : (
-            <SelectionList>
-              {Object.keys(theme.characters.selectMoveMapping).map(key => {
-                const Component = theme.characters.selectMoveMapping[key];
-                return (
-                  <SelectionListItem key={key}>
-                    <Component onSelection={onSelection} />
-                  </SelectionListItem>
-                );
-              })}
-            </SelectionList>
-          )
-        }
-      </FeatureToggle>
+      <PageSubTitle>Make your move 做你的動作</PageSubTitle>
+      <ListContainer>
+        <ItemCardSelection items={items} onItemSelected={selectCharacter} />
+      </ListContainer>
     </React.Fragment>
   );
 };
