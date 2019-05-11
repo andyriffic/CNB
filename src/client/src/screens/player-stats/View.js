cc084f76e17e7efc017b48cf8a562848ca33fff9
FooDog <andyriffic@gmail.com>
2019-05-11 17:17:03 +1000

    Add fade-in animation for leaderboard rankings

--- src/client/src/screens/player-stats/View.js
+++ src/client/src/screens/player-stats/View.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import FullPage from '../../components/page-layout/FullPage';
 import { PageSubTitle } from '../styled';
 import { STATS_API_BASE_URL } from '../../environment';
@@ -16,6 +16,11 @@ const fetchRankings = () => {
   });
 };
 
+const RankingItemEnterAnimation = keyframes`
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+`;
+
 const RankingScrollableContainer = styled.div`
   max-height: 65vh;
   overflow-y: scroll;
@@ -30,25 +35,33 @@ const RankingContainer = styled.div`
 const RankingItem = styled.div`
   display: grid;
   grid-template-columns: 25% 50% 25%;
-  background-color: #1e0f1c;
+  background-color: rgba(30, 15, 28, 0.8);
   border-radius: 7px;
   margin-bottom: 10px;
   align-items: center;
   padding: 5px 0;
-  opacity: 0.8;
-  font-size: 0.8rem;
+  font-size: 0.6rem;
+  animation: ${RankingItemEnterAnimation} 1s ease-in
+    ${props => (props.position > 10 ? 0 : 10 - props.position) * 700}ms 1 both;
   ${props =>
-    props.feature && 'font-size: 1.2rem; font-weight: bold; opacity: 1;'}
+    props.feature &&
+    'font-size: 1.2rem; font-weight: bold; background-color: rgba(30,15,28, 1);'}
 `;
 const RankingPlace = styled.div`
   color: #fff;
+  opacity: 0.8;
+  ${props => props.feature && 'opacity: 1;'}
 `;
 const RankingPlayerName = styled.div`
   color: #e2e9c0;
+  opacity: 0.8;
+  ${props => props.feature && 'opacity: 1;'}
 `;
 const RankingScore = styled.div`
   justify-self: center;
   color: #7aa95c;
+  opacity: 0.8;
+  ${props => props.feature && 'opacity: 1;'}
 `;
 
 const Link = styled.a`
@@ -88,12 +101,21 @@ const View = () => {
       <RankingScrollableContainer>
         <RankingContainer>
           {rankingList.result.map((ranking, index) => {
+            if (ranking.player === 'Guest') {
+              // TODO: filter guest out on server
+              return null;
+            }
             const isFirst = index === 0;
             const firstEqual =
               ranking.times_won === rankingList.result[0].times_won;
             const featureRow = isFirst || firstEqual;
             return (
-              <RankingItem feature={featureRow} key={ranking.player}>
+              <RankingItem
+                feature={featureRow}
+                key={ranking.player}
+                totalRankings={rankingList.result.length}
+                position={index}
+              >
                 <RankingPlace
                   feature={featureRow}
                   style={{ textAlign: 'center' }}
