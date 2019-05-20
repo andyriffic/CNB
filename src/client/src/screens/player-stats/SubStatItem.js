16804f7807c4610b8bb52bf602ebc7f2804fb575
FooDog <andyriffic@gmail.com>
2019-05-20 17:57:35 +1000

    Add win/loss/draw stats to leaderboard display

---
+++ src/client/src/screens/player-stats/SubStatItem.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReadableNumberFont } from '../../components/ReadableNumberFont';
+
+const Container = styled.dl`
+  margin: 0;
+  padding: 4px 8px;
+  background-color: ${props => props.style.bgColor};
+  color: white;
+  border-radius: 7px;
+`;
+
+const Title = styled.dt`
+  font-size: 0.4rem;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+`;
+
+const Value = styled.dd`
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  line-height: 1;
+`;
+
+const styles = {
+  wins: {
+    bgColor: '#5cb85c',
+  },
+  losses: {
+    bgColor: '#DF562C',
+  },
+  draws: {
+    bgColor: '#E99D44',
+  },
+};
+
+export const SubStatItem = ({ title, value, type = 'wins' }) => {
+  return (
+    <Container style={styles[type]}>
+      <Title>{title}</Title>
+      <Value>
+        <ReadableNumberFont>{value}</ReadableNumberFont>
+      </Value>
+    </Container>
+  );
+};
