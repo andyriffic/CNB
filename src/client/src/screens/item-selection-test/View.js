b60efb139996086c6c333688b58966e8d4bd4c10
FooDog <andyriffic@gmail.com>
2019-05-21 19:37:32 +1000

    Refactor waiting-indicator into it's own component

--- src/client/src/screens/item-selection-test/View.js
+++ src/client/src/screens/item-selection-test/View.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import FullPage from '../../components/page-layout/FullPage';
 import ItemSelection from '../../components/item-card-selection';
+import WaitingIndicator from '../../components/waiting-indicator';
 
 const Item = styled.p`
   border: 2px solid black;
@@ -24,76 +25,13 @@ const Container = styled.div`
   border: 2px solid white;
 `;
 
-const SpinAnimation = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const SpinningBorderContent = styled.div`
-  width: 24vmin;
-  height: 24vmin;
-  position: relative;
-`;
-
-const LoadingCircle = styled.svg`
-  z-index: 1;
-  animation: ${SpinAnimation} 2s linear infinite;
-  #track {
-    transition: fill 1s ease-in-out;
-    fill: ${props => (props.loaded ? '#4F8A10' : '#BDE5F8')};
-  }
-
-  #section {
-    transition: fill 1s ease-in-out;
-    fill: ${props => (props.loaded ? '#4F8A10' : '#00529B')};
-  }
-`;
-
-const LoadingContent = styled.div`
-  background-color: ${props => (props.loaded ? 'white' : 'white')};
-  position: absolute;
-  top: 1px;
-  left: 1px;
-  right: 1px;
-  bottom: 1px;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: background-color 1s ease-in-out;
-  text-align: center;
-  font-size: 0.5rem;
-`;
-
 const View = () => {
   const [focusItemIndex, setFocusItemIndex] = useState(0);
   const [loaded, setLoaded] = useState(false);
   return (
     <FullPage pageTitle="Item selection test">
-      <SpinningBorderContent loaded={loaded} onClick={() => setLoaded(!loaded)}>
-        <LoadingContent loaded={loaded}>Waiting for Player</LoadingContent>
-        <LoadingCircle
-          loaded={loaded}
-          id="loading-spinner"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 48 48"
-        >
-          <g fill="none">
-            <path
-              id="track"
-              d="M24,48 C10.745166,48 0,37.254834 0,24 C0,10.745166 10.745166,0 24,0 C37.254834,0 48,10.745166 48,24 C48,37.254834 37.254834,48 24,48 Z M24,44 C35.045695,44 44,35.045695 44,24 C44,12.954305 35.045695,4 24,4 C12.954305,4 4,12.954305 4,24 C4,35.045695 12.954305,44 24,44 Z"
-            />
-            <path
-              id="section"
-              d="M24,0 C37.254834,0 48,10.745166 48,24 L44,24 C44,12.954305 35.045695,4 24,4 L24,0 Z"
-            />
-          </g>
-        </LoadingCircle>
-      </SpinningBorderContent>
+      <button onClick={() => setLoaded(!loaded)}>toggle</button>
+      <WaitingIndicator loaded={loaded}>HELLO!</WaitingIndicator>
       <div>Focus Item: {focusItemIndex}</div>
       <Container>
         <ItemSelection
--- src/client/src/screens/spectator-screen/components/player-status/View.js
+++ src/client/src/screens/spectator-screen/components/player-status/View.js
@@ -1,9 +1,10 @@
 /* @flow */
 import React from 'react';
 import type { ComponentType } from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 import TranslatedPlayerName from '../../../../components/translated-player-name';
+import WaitingIndicator from '../../../../components/waiting-indicator';
 
 type Props = {
   moved: boolean,
@@ -11,42 +12,6 @@ type Props = {
   badge?: ComponentType<*>,
   avatar: Object,
 };
-
-const Rainbow = keyframes`
-  0% { background-position:0% 82%; }
-  50% { background-position:100% 19%; }
-  100% { background-position:0% 82%; }
-`;
-
-const Container = styled.div`
-  background-color: #ffb758;
-  border-radius: 20px;
-  height: 24vmin;
-  width: 24vmin;
-  display: flex;
-  position: relative;
-  align-items: center;
-  justify-content: center;
-  transition: opacity 1s linear;
-  opacity: ${props => (props.moved ? '1' : '0.5')};
-  &.moved {
-    background: linear-gradient(
-      124deg,
-      #ff2400,
-      #e81d1d,
-      #e8b71d,
-      #e3e81d,
-      #1de840,
-      #1ddde8,
-      #2b1de8,
-      #dd00f3,
-      #dd00f3
-    );
-    background-size: 1800% 1800%;
-    animation: ${Rainbow} 10s ease infinite;
-    box-shadow: 0 0 100px #ffffff;
-  }
-`;
 
 const Title = styled.div`
   margin: 0;
@@ -62,106 +27,17 @@ const BadgeContainer = styled.div`
   opacity: 0.7;
 `;
 
-const SpinAnimation = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const SpinningBorderContent = styled.div`
-  width: 24vmin;
-  height: 24vmin;
-  position: relative;
-`;
-
-const LoadingCircle = styled.svg`
-  z-index: 1;
-  animation: ${SpinAnimation} 2s linear infinite;
-  #track {
-    transition: fill 1s ease-in-out;
-    fill: ${props => (props.loaded ? '#4F8A10' : '#BDE5F8')};
-  }
-
-  #section {
-    transition: fill 1s ease-in-out;
-    fill: ${props => (props.loaded ? '#4F8A10' : '#00529B')};
-  }
-`;
-
-const LoadingContent = styled.div`
-  background-color: #FDFDF0;
-  position: absolute;
-  top: 1px;
-  left: 1px;
-  right: 1px;
-  bottom: 1px;
-  border-radius: 50%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  transition: background-color 1s ease-in-out 1s;
-  text-align: center;
-  font-size: 0.5rem;
-  /* &.moved {
-    background: linear-gradient(
-      124deg,
-      #ff2400,
-      #e81d1d,
-      #e8b71d,
-      #e3e81d,
-      #1de840,
-      #1ddde8,
-      #2b1de8,
-      #dd00f3,
-      #dd00f3
-    );
-    background-size: 1800% 1800%;
-    animation: ${Rainbow} 10s ease infinite;
-    //box-shadow: 0 0 100px #ffffff;
-  } */
-`;
-
 const View = ({ moved, name, badge, avatar }: Props) => {
   return (
-    // <Container moved={moved} className={moved ? 'moved' : ''}>
-    //   <Title>
-    //     <TranslatedPlayerName playerName={name} />
-    //     <p>{avatar && avatar.name}</p>
-    //   </Title>
-    //   {badge && <BadgeContainer>{badge}</BadgeContainer>}
-    // </Container>
-    <SpinningBorderContent loaded={moved}>
-      <LoadingContent loaded={moved} className={moved && 'moved'}>
-        <Title>
-          <TranslatedPlayerName playerName={name} />
-        </Title>
-        <p style={{ margin: '0', padding: '0' }}>
-          {avatar ? `${avatar.name} ✅` : 'Waiting...'}
-        </p>
-        {badge && <BadgeContainer>{badge}</BadgeContainer>}
-      </LoadingContent>
-      <LoadingCircle
-        loaded={moved}
-        id="loading-spinner"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 48 48"
-      >
-        <g fill="none">
-          <path
-            id="track"
-            d="M24,48 C10.745166,48 0,37.254834 0,24 C0,10.745166 10.745166,0 24,0 C37.254834,0 48,10.745166 48,24 C48,37.254834 37.254834,48 24,48 Z M24,44 C35.045695,44 44,35.045695 44,24 C44,12.954305 35.045695,4 24,4 C12.954305,4 4,12.954305 4,24 C4,35.045695 12.954305,44 24,44 Z"
-          />
-          <path
-            id="section"
-            d="M24,0 C37.254834,0 48,10.745166 48,24 L44,24 C44,12.954305 35.045695,4 24,4 L24,0 Z"
-          />
-        </g>
-      </LoadingCircle>
-    </SpinningBorderContent>
+    <WaitingIndicator loaded={moved}>
+      <Title>
+        <TranslatedPlayerName playerName={name} />
+      </Title>
+      <p style={{ margin: '0', padding: '0' }}>
+        {avatar ? `${avatar.name} ✅` : 'Waiting...'}
+      </p>
+      {badge && <BadgeContainer>{badge}</BadgeContainer>}
+    </WaitingIndicator>
   );
 };
 
