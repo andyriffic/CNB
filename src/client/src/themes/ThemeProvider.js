0994e8a161df48c3dc8536f959864821f3aeaac2
FooDog <andyriffic@gmail.com>
2019-07-02 18:57:24 +1000

    client lint

--- src/client/src/themes/ThemeProvider.js
+++ src/client/src/themes/ThemeProvider.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import GameThemeContext from '../contexts/GameThemeContext';
 import PandaPizzaPirateTheme from './panda-pizza-pirate';
-import { getThemeForDate } from './theme-providers';
 
 const View = ({ children }) => {
-  const today = new Date();
   let theme = PandaPizzaPirateTheme; // Hard-code until we hook into the server for the theme
   console.log('Setting theme', theme);
 
