3b10e39ed2029d458fce9ff716a9b42434bd8e5e
Andrew Bell <andrew.bell@rea-group.com>
2019-07-02 13:29:00 +1000

    Set theme to be PPP all the time

--- src/client/src/themes/ThemeProvider.js
+++ src/client/src/themes/ThemeProvider.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import GameThemeContext from '../contexts/GameThemeContext';
+import PandaPizzaPirateTheme from './panda-pizza-pirate';
 import { getThemeForDate } from './theme-providers';
 
 const View = ({ children }) => {
   const today = new Date();
-  let theme = getThemeForDate(today);
+  let theme = PandaPizzaPirateTheme; // Hard-code until we hook into the server for the theme
   console.log('Setting theme', theme);
 
   return (
