306f2c02e9b7175aa234e7fce757d00c9d24996e
FooDog <andyriffic@gmail.com>
2019-08-20 20:15:18 +1000

    Remove feature toggle

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import UpliftApp from './uplift';
 import UpliftTest from './socket-uplift/test-view';
 import UpliftSpectatorApp from './socket-uplift/test-spectator-view';
 import UpliftPlayerApp from './socket-uplift/test-player-view';
 import { isFeatureEnabled } from './featureToggle';
 
-if (isFeatureEnabled('uplift')) {
-  ReactDOM.render(<UpliftApp />, document.getElementById('root'));
-} else if (isFeatureEnabled('uplift-test')) {
+if (isFeatureEnabled('uplift-test')) {
   ReactDOM.render(<UpliftTest />, document.getElementById('root'));
 } else if (isFeatureEnabled('uplift-spectator')) {
   ReactDOM.render(<UpliftSpectatorApp />, document.getElementById('root'));
 } else if (isFeatureEnabled('uplift-player')) {
   ReactDOM.render(<UpliftPlayerApp />, document.getElementById('root'));
 } else {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(<UpliftApp />, document.getElementById('root'));
 }
