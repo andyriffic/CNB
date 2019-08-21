a2cc7d5efa5ae22a2142dc0a2cf19f2d77b619c7
Andrew Bell <andrew.bell@rea-group.com>
2019-08-21 14:31:25 +1000

    Add test teams

--- src/server/src/uplift/services/player/constants.ts
+++ src/server/src/uplift/services/player/constants.ts
@@ -44,6 +44,8 @@ export const ALL_TEAMS = [
   { id: 'go_duck', name: 'Go Duck' },
   { id: 'a_team', name: 'The A Team' },
   { id: 'bake_my_day', name: 'Bake My Day' },
+  { id: 'developers', name: 'Developers' },
+  { id: 'bas', name: 'Business Analysts' },
 ];
 
 export const PLAYER_IDS_BY_TEAM: { [teamId: string]: string[] } = {
@@ -128,4 +130,6 @@ export const PLAYER_IDS_BY_TEAM: { [teamId: string]: string[] } = {
   'go_duck': ['duyen', 'yixing'],
   'a_team': ['albert', 'andy'],
   'bake_my_day': ['michael_b', 'yujin'],
+  'developers': ['michael_b'],
+  'bas': ['alex'],
 };
