2f5570a7e185815f9a0f9a37e875e980d8a0d75d
FooDog <andyriffic@gmail.com>
2019-09-03 19:22:42 +1000

    Remove unused useState

--- src/client/src/game-settings/components/sound-toggle/View.js
+++ src/client/src/game-settings/components/sound-toggle/View.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import GameSettingsContext from '../../../contexts/GameSettingsContext';
 import RadioSelectList from '../../../components/form/radio-select';
 
