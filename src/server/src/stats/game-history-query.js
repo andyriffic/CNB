d7ae9e6a191963da1dc1689bba872eb45bdebbde
FooDog <andyriffic@gmail.com>
2019-09-20 16:05:32 +1000

    Add team names to game history athena query

--- src/server/src/stats/game-history-query.js
+++ src/server/src/stats/game-history-query.js
@@ -1,8 +1,10 @@
 export const gameHistoryQuery = `SELECT date,
-theme AS theme,
+         theme AS theme,
          result.winner AS winner,
          player1.player AS player1,
+         player1.team AS team1,
          player2.player AS player2,
+         player2.team AS team2,
     CASE
       WHEN result.draw THEN true
       ELSE NULL
