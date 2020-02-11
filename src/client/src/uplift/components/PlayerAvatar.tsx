09fddc5d29fd4d7573304bf949306d90bd9b88b2
FooDog <andyriffic@gmail.com>
2020-02-11 21:33:40 +1100

    Add player 'fade away' feature

--- src/client/src/uplift/components/PlayerAvatar.tsx
+++ src/client/src/uplift/components/PlayerAvatar.tsx
@@ -2,17 +2,33 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import { Player } from '../contexts/PlayersProvider';
 import { SOCKETS_ENDPOINT } from '../../environment';
+import { getPlayerAttributeValue } from '../utils/player';
+import moment from 'moment';
 
 export type PlayerAvatarPosition = 'left' | 'right';
+
+const getDaysRemaining = (tags: string[]): number | undefined => {
+  const dateLeaving = getPlayerAttributeValue(tags, 'leaving', '');
+
+  if (dateLeaving === '') {
+    return;
+  }
+
+  const dateMoment = moment(dateLeaving).utc();
+  var diffInDays = dateMoment.diff(moment().utc(), 'days');
+  return diffInDays >= 0 ? diffInDays : undefined;
+};
 
 const StyledPlayer = styled.img<{
   position: PlayerAvatarPosition;
   overrideStyle: string;
+  opacity: number;
 }>`
   width: 20vmin;
   height: 30vmin;
   ${props => props.position === 'right' && 'transform: scaleX(-1);'}
   ${props => props.overrideStyle};
+  opacity: ${props => props.opacity};
 `;
 
 type PlayerAvatarProps = {
@@ -26,9 +42,14 @@ export const PlayerAvatar = ({
   position = 'left',
   overrideStyle = '',
 }: PlayerAvatarProps) => {
+  const daysLeft = getDaysRemaining(player.tags);
+  const fadeAwayOpacity =
+    daysLeft === undefined ? 1 : 0.4 + (daysLeft * 2) / 100;
+
   return (
     <StyledPlayer
       position={position}
+      opacity={fadeAwayOpacity}
       src={`${SOCKETS_ENDPOINT}${player.avatarImageUrl}`}
       overrideStyle={overrideStyle}
     ></StyledPlayer>
