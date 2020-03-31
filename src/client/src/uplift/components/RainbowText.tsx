3c25fe8a26f50901fe1160e4629afae8c7a4aabe
FooDog <andyriffic@gmail.com>
2020-03-31 17:59:00 +1100

    Basic player selction working for invitations

---
+++ src/client/src/uplift/components/RainbowText.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+// const Rainbow = keyframes`
+//   0% {color: orange;}
+//   10% {color: purple;}
+// 	20% {color: red;}
+//   30% {color: CadetBlue;}
+// 	40% {color: yellow;}
+//   50% {color: coral;}
+// 	60% {color: green;}
+//   70% {color: cyan;}
+//   80% {color: DeepPink;}
+//   90% {color: DodgerBlue;}
+//   100% {color: orange;}
+// `;
+
+export const Rainbow = keyframes`
+  0% {color: #fea3aa;}	
+  20% {color: #f8b88b;}	
+	40% {color: #faf884;}
+  60% {color: #baed91;}
+	80% {color: #b2cefe;}
+  100% {color: #f2a2e8;}
+`;
+
+const Container = styled.span`
+  text-shadow: 2px 2px #000000;
+  animation: ${Rainbow} 8s infinite;
+`;
+
+export const RainbowText = ({ children }: { children: React.ReactNode }) => {
+  return <Container>{children}</Container>;
+};
---
+++ src/client/src/uplift/screens/invitation-matchup/StatusIndicator.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled, { keyframes, css } from 'styled-components';
+
+export enum STATUS_TYPE {
+  WAITING = 'waiting',
+  READY = 'ready',
+}
+
+const readyBackgroundAnimation = keyframes`
+  0% {background: #008000;}
+  25%  {background: #00A000;}
+  50%  {background: #00B800;}
+  75%  {background: #00D000;}
+  100% {background: #008000;}
+`;
+
+const waitingBackgroundAnimation = keyframes`
+  0% {background: #000080;}
+  25%  {background: #0000A0;}
+  50%  {background: #0000B0;}
+  75%  {background: #0000C8;}
+  100% {background: #000080;}
+`;
+
+const Container = styled.div<{ status: STATUS_TYPE }>`
+  border-radius: 8px;
+  font-size: 1rem;
+  ${props =>
+    props.status === STATUS_TYPE.READY &&
+    css`
+      animation: ${readyBackgroundAnimation} 8s linear infinite;
+    `}
+  ${props =>
+    props.status === STATUS_TYPE.WAITING &&
+    css`
+      animation: ${waitingBackgroundAnimation} 8s linear infinite;
+    `}
+`;
+
+type Props = {
+  status?: STATUS_TYPE;
+  children: React.ReactNode;
+};
+
+export const StatusIndicator = ({
+  status = STATUS_TYPE.WAITING,
+  children,
+}: Props) => {
+  return <Container status={status}>{children}</Container>;
+};
