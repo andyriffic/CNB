5d2170dee8b7e0c280202645befe3bd06c83588c
FooDog <andyriffic@gmail.com>
2020-04-05 15:17:33 +1000

    SHow game result on players screen

--- src/client/src/uplift/screens/play/PlayMatchupView.tsx
+++ src/client/src/uplift/screens/play/PlayMatchupView.tsx
@@ -4,16 +4,17 @@ import { RouteComponentProps } from '@reach/router';
 import { FullPageScreenLayout } from '../../components/layouts/FullPageScreenLayout';
 import { usePlayer } from './usePlayer';
 import { usePlayerMatchups } from './usePlayerMatchups';
-import { MatchupContext } from '../../contexts/MatchupProvider';
+import { MatchupContext, GAME_STATUS } from '../../contexts/MatchupProvider';
 import { SelectMove } from './components/SelectMove';
 import { GameThemeContext } from '../../contexts/ThemeProvider';
+import { PlayerGameResult } from './components/PlayerGameResult';
 
 type Props = {
   playerId: string;
   matchupId: string;
 } & RouteComponentProps;
 
-export const PlayMatchupView = ({ playerId, matchupId }: Props) => {
+export const PlayMatchupView = ({ playerId, matchupId, navigate }: Props) => {
   const player = usePlayer(playerId);
   const { setTheme } = useContext(GameThemeContext);
   const playerMatchups = usePlayerMatchups(playerId);
@@ -44,15 +45,24 @@ export const PlayMatchupView = ({ playerId, matchupId }: Props) => {
       alignTop={true}
       scrollable={false}
     >
-      {player && matchup && (
-        <>
+      {player &&
+        matchup &&
+        matchup.gameInProgress &&
+        (matchup.gameInProgress.status === GAME_STATUS.Finished ? (
+          <PlayerGameResult
+            matchup={matchup}
+            teamId={matchup.playerTeamId}
+            backToMatchups={() => {
+              navigate && navigate('/play');
+            }}
+          />
+        ) : (
           <SelectMove
             matchupId={matchupId}
             player={player}
             teamId={matchup.playerTeamId}
           />
-        </>
-      )}
+        ))}
     </FullPageScreenLayout>
   );
 };
