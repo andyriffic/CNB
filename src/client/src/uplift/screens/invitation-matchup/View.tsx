0bdfdb3d4cbc6b6bdb24f90877cca844e24d9968
FooDog <andyriffic@gmail.com>
2020-04-19 15:01:08 +1000

    Give each player a snakes and ladders move when starting matchup

--- src/client/src/uplift/screens/invitation-matchup/View.tsx
+++ src/client/src/uplift/screens/invitation-matchup/View.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react';
 import styled from 'styled-components';
 import { RouteComponentProps } from '@reach/router';
-import { Player } from '../../contexts/PlayersProvider';
+import { Player, PlayersContext } from '../../contexts/PlayersProvider';
 import { FullPageScreenLayout } from '../../components/layouts/FullPageScreenLayout';
 import { MatchupContext } from '../../contexts/MatchupProvider';
 import { RandomPlayers } from './RandomPlayers';
@@ -17,6 +17,7 @@ import {
 } from '../../contexts/InvitationsProvider';
 import { SOUND_KEYS } from '../../../sounds/SoundService';
 import { SplashText } from '../../components/SplashText';
+import { getPlayerAttributeValue } from '../../utils/player';
 
 const MatchupsContainer = styled.div`
   width: 1200px;
@@ -45,6 +46,7 @@ export default ({ navigate }: RouteComponentProps) => {
     shownTitle: false,
     bothPlayersSelected: false,
   });
+  const { updatePlayer } = useContext(PlayersContext);
 
   const updateViewState = (updatedViewState: Partial<ViewState>) => {
     setViewState({ ...viewState, ...updatedViewState });
@@ -133,6 +135,19 @@ export default ({ navigate }: RouteComponentProps) => {
         <SplashText
           onComplete={() => {
             soundService.stop(SOUND_KEYS.PLAYER_SELECT_MUSIC);
+            updatePlayer(player1!.id, [
+              ...player1!.tags.filter(t => !t.startsWith('sl_moves')),
+              `sl_moves:${parseInt(
+                getPlayerAttributeValue(player1!.tags, 'sl_moves', '0')
+              ) + 1}`,
+            ]);
+            updatePlayer(player2!.id, [
+              ...player2!.tags.filter(t => !t.startsWith('sl_moves')),
+              `sl_moves:${parseInt(
+                getPlayerAttributeValue(player2!.tags, 'sl_moves', '0')
+              ) + 1}`,
+            ]);
+
             addInstantMatchup(
               player1!.id,
               player2!.id,
