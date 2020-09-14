86af27823f54ac09abfdbca4e564b98e36d041c4
FooDog <andyriffic@gmail.com>
2020-09-14 18:53:21 +1000

    Animate points when changed

--- src/client/src/v2/screens/spectator/Points.tsx
+++ src/client/src/v2/screens/spectator/Points.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useRef, useState } from 'react';
+import styled, { css } from 'styled-components';
 import { pulseAnimation } from '../../../uplift/components/animations';
 import { ReadableNumberFont } from '../../../components/ReadableNumberFont';
 
-const Container = styled.div`
+const Container = styled.div<{ animate: boolean }>`
   width: 5vw;
   height: 5vw;
   background-color: red;
@@ -19,7 +19,11 @@ const Container = styled.div`
   justify-content: center;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
   box-sizing: border-box;
-  /* animation: ${pulseAnimation} 3s infinite; */
+  ${({ animate }) =>
+    animate &&
+    css`
+      animation: ${pulseAnimation} 500ms;
+    `}
 `;
 
 const Title = styled.div`
@@ -33,8 +37,18 @@ type Props = {
 };
 
 export const Points = ({ title, value }: Props) => {
+  const previousPoints = useRef(value);
+  const [playAnimation, setPlayAnimation] = useState(false);
+
+  useEffect(() => {
+    if (value !== previousPoints.current) {
+      previousPoints.current = value;
+      setPlayAnimation(true);
+    }
+  }, [value]);
+
   return (
-    <Container>
+    <Container animate={playAnimation}>
       {title && <Title>{title}</Title>}
       <ReadableNumberFont>{value}</ReadableNumberFont>{' '}
     </Container>
