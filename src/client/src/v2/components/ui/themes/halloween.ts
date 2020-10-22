381fcb7e3605da4a3ce9242a9a7c93db956ec182
FooDog <andyriffic@gmail.com>
2020-10-22 17:03:27 +1100

    Add border color override for halloween theme

--- src/client/src/v2/components/ui/themes/halloween.ts
+++ src/client/src/v2/components/ui/themes/halloween.ts
@@ -10,6 +10,7 @@ const theme: ThemeStyles = {
     ...defaultTheme.color,
     text01: '#5E32BA',
     text03: '#BFDA7A',
+    border01: '#E9804D',
     background01: '#cccccc',
     background02: '#666666',
     background03: '#EB6123',
