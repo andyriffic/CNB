f7be02b8ed146707481b99158977e9843151ea79
FooDog <andyriffic@gmail.com>
2020-10-24 17:27:19 +1100

    Update halloween sounds

--- src/client/src/v2/sounds/halloween/index.ts
+++ src/client/src/v2/sounds/halloween/index.ts
@@ -6,15 +6,18 @@ import halloweenMusic from './assets/halloween_music.mp3';
 import screamingSheep from './assets/screaming_sheep.mp3';
 import ghostLaugh from './assets/ghost_laugh.mp3';
 import ghostsAndGoblins from './assets/ghosts_n_goblins.mp3';
+import ghost from './assets/ghost.mp3';
+import funeralMarch from './assets/funeral_march.mp3';
 
 const soundMap: SoundMap = {
   ...defaultSounds,
   WaitForPlayersToJoin: halloweenAngryBird,
   PlayerJoinedGame: ghostLaugh,
   SwitchPlayer: screamingSheep,
+  RoundStart: ghost,
   WaitForMoves: halloweenMusic,
   PlayerMoved: screamingSheep,
-  GameOver: ghostsAndGoblins,
+  GameOver: funeralMarch,
 };
 
 export default soundMap;
