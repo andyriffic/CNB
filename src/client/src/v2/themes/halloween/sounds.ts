5797d34d16fc49a85e8284b4a4eec0dff8365210
FooDog <andyriffic@gmail.com>
2021-10-17 09:44:19 +1100

    Update halloween theme with mob sounds

--- src/client/src/v2/themes/halloween/sounds.ts
+++ src/client/src/v2/themes/halloween/sounds.ts
@@ -8,16 +8,26 @@ import ghostLaugh from './sounds/ghost_laugh.mp3';
 import ghostsAndGoblins from './sounds/ghosts_n_goblins.mp3';
 import ghost from './sounds/ghost.mp3';
 import funeralMarch from './sounds/funeral_march.mp3';
+import thriller from './sounds/thriller.mp3';
+import boosLaugh from './sounds/boos_laugh.mp3';
+import thrillerScream from './sounds/thriller_scream.mp3';
+import thrillerSong from './sounds/thriller_song.mp3';
 
 const soundMap: SoundMap = {
   ...defaultSounds,
   WaitForPlayersToJoin: halloweenAngryBird,
   PlayerJoinedGame: ghostLaugh,
-  SwitchPlayer: screamingSheep,
+  // SwitchPlayer: screamingSheep,
   RoundStart: ghost,
   WaitForMoves: halloweenMusic,
-  PlayerMoved: screamingSheep,
+  // PlayerMoved: screamingSheep,
   GameOver: funeralMarch,
+  ChoseMobMusic: halloweenMusic,
+  MobWaitingMovesMusic: halloweenMusic,
+  PlayerJoinedMob: boosLaugh,
+  MugChosen: thrillerScream,
+  MobWinsMusic: thrillerSong,
+  MugWinsMusic: thrillerSong,
 };
 
 export default soundMap;
