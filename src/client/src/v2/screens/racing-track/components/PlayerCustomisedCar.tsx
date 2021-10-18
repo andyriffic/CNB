b98db2d0e129012814054685f865cee993f600c7
FooDog <andyriffic@gmail.com>
2021-10-18 21:05:02 +1100

    Set racing car to packman ghost for halloween

--- src/client/src/v2/screens/racing-track/components/PlayerCustomisedCar.tsx
+++ src/client/src/v2/screens/racing-track/components/PlayerCustomisedCar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { RacingPlayer } from '../types';
 import { BubbleCar } from './racing-cars/BubbleCar';
 import { FormulaOneCar } from './racing-cars/FormulaOneCar';
+import { PacManGhost } from './racing-cars/PacManGhost';
 import { SportsCar } from './racing-cars/SportsCar';
 
 type Props = {
@@ -15,7 +16,7 @@ const getCustomCarStyle = (racingPlayer: RacingPlayer): JSX.Element => {
     case 'bubble':
       return <BubbleCar color={racingPlayer.carColor} />;
     default:
-      return <SportsCar color={racingPlayer.carColor} />;
+      return <PacManGhost color={racingPlayer.carColor} />; //<SportsCar color={racingPlayer.carColor} />;
   }
 };
 
