ac6f9a53c8c6b180c1b5607f8076011ab3c7c292
FooDog <andyriffic@gmail.com>
2021-10-27 15:21:01 +1100

    wrap race-track section debug behind feature toggle

--- src/client/src/v2/screens/racing-track/components/RacingSegment.tsx
+++ src/client/src/v2/screens/racing-track/components/RacingSegment.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { isFeatureEnabled } from '../../../../featureToggle';
 import { RacingTrackSection } from '../types';
 import { RacingLane } from './RacingLane';
 
@@ -12,17 +13,21 @@ type Props = {
   index: number;
 };
 
+const debug = isFeatureEnabled('debug');
+
 export const RacingSegment = ({ section, index }: Props): JSX.Element => {
   return (
     <>
-      <PositionContainer
-        style={{
-          top: `${section.lanes[0].squares[0].coordinates.y}px`,
-          left: `${section.lanes[0].squares[0].coordinates.x}px`,
-        }}
-      >
-        {index}
-      </PositionContainer>
+      {debug && (
+        <PositionContainer
+          style={{
+            top: `${section.lanes[0].squares[0].coordinates.y}px`,
+            left: `${section.lanes[0].squares[0].coordinates.x}px`,
+          }}
+        >
+          {index}
+        </PositionContainer>
+      )}
       {section.lanes.map((lane, i) => (
         <RacingLane key={i} lane={lane} section={section} />
       ))}
