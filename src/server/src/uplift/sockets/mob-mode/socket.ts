57bd39112ef4e643dc74e444d10968d559e62b9c
FooDog <andyriffic@gmail.com>
2023-03-20 17:40:11 +1100

    Update game points to donkey-kong

--- src/server/src/uplift/sockets/mob-mode/socket.ts
+++ src/server/src/uplift/sockets/mob-mode/socket.ts
@@ -14,7 +14,7 @@ import { Player } from '../../services/player/types';
 import { mapMobGameToStats } from '../../services/stats/mobMappers';
 import { StatsService } from '../../services/stats';
 import { playersDatastore } from '../../datastore/players';
-import { pointsToPlayersPacman as pointsToPlayer } from './points-to-player-pacman';
+import { pointsToPlayersKong as pointsToPlayer } from './points-to-player-kong';
 
 const REQUEST_MOB_GAMES = 'REQUEST_MOB_GAMES';
 const MOB_GAMES_UPDATE = 'MOB_GAMES_UPDATE';
